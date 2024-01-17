4c3a22a506b109f8f52ac3cb40616d4dc0efdbe2
Cynthia Barasa <109290742+CynthiaBarasa@users.noreply.github.com>
2024-01-17 11:32:55 +0300

    Update Rotate Image Using OpenCV.py

--- Rotate Image Using OpenCV.py
+++ Rotate Image Using OpenCV.py
@@ -1,1 +1,11 @@
+import cv2
 
+img = cv2.imread("face.jpeg")
+height , width = img.shape[: 2]
+rotateimg = cv2.getRotationMatrix2D((width / 2, height / 2), 70, .5)
+finalimg = cv2.warpAffine(img, rotateimg, (width, height))
+cv2.imshow("Rotated Image", finalimg)
+cv2.imshow("Original", img)
+
+
+cv2.waitKey(0)
