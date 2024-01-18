f28c4bd1789388170a2ad6ac947f5a758b0431d4
Cynthia Barasa <109290742+CynthiaBarasa@users.noreply.github.com>
2024-01-18 12:23:30 +0300

    Update Crop Image Using OpenCV.py

--- Crop Image Using OpenCV.py
+++ Crop Image Using OpenCV.py
@@ -1,1 +1,12 @@
+import cv2
 
+img = cv2.imread("face.jpeg")
+height, width = img.shape[: 2]
+start_row, start_col = int(height * .15), int(width * .15)
+end_row, end_col = int(height * .65), int(width * .65)
+
+cropped = img[start_row: end_row, start_col: end_col]
+cv2.imshow("Cropped", cropped)
+cv2.imshow("Original", img)
+
+cv2.waitKey(0)
