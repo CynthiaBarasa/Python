6b6318c488275a8c6d722f53939fe174039db0ac
Cynthia Barasa <109290742+CynthiaBarasa@users.noreply.github.com>
2024-01-18 12:33:03 +0300

    Update Image Pyramid Using OpenCV.py

--- Image Pyramid Using OpenCV.py
+++ Image Pyramid Using OpenCV.py
@@ -1,1 +1,11 @@
+import cv2
 
+img = cv2.imread("face.jpeg")
+smaller = cv2.pyrDown(img)
+larger = cv2.pyrUp(img)
+
+cv2.imshow("Original", img)
+cv2.imshow("Larger", larger)
+cv2.imshow("Smaller", smaller)
+
+cv2.waitKey(0)
