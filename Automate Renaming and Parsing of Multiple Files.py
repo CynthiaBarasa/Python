d8905de659c77c847497234e3c95f4fb9b6e0495
Cynthia Barasa <109290742+CynthiaBarasa@users.noreply.github.com>
2024-01-20 00:29:45 +0300

    Update Automate Renaming and Parsing of Multiple Files.py

--- Automate Renaming and Parsing of Multiple Files.py
+++ Automate Renaming and Parsing of Multiple Files.py
@@ -1,1 +1,14 @@
+import os
+os.chdir('C:/Users/User/Downloads/Documents')
+#print(os.getcwd())
+for f in os.listdir():
+   f_name, f_ext =(os.path.splitext(f))
+   f_course, f_title, f_number = (f_name.split('-'))
 
+   f_course = f_course.strip()
+   f_title = f_title.strip() 
+   f_number = f_number.strip()[1:].zfill(2)
+
+   new_file = ('{}-{}-{}{}', f_number, f_title, f_course, f_ext)
+
+   os.rename(f, new_file)
