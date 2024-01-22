bf80fb46696e1016c38878470b62247302ea798b
Cynthia Barasa <109290742+CynthiaBarasa@users.noreply.github.com>
2024-01-22 23:55:14 +0300

    Update Snake Game.py

--- Snake Game.py
+++ Snake Game.py
@@ -1,16 +1,16 @@
-#Importing Libraries
+# Importing Libraries
 import turtle
 import random
 import time
 
-#Creating Screen
+# Creating Screen
 screen = turtle.Screen()
 screen.title("SNAKE GAME")
 screen.setup(width=700, heigh=700)
 screen.tracer(0)
 screen.bgcolor("#1d1d1d")
 
-#Creating Border
+# Creating Border
 turtle.speed(5)
 turtle.pensize(4)
 turtle.penup()
@@ -41,3 +41,37 @@ snake.goto(0, 0)
 snake.direction = 'stop'
 
 # Food
+fruit=turtle.Turtle()
+fruit.speed(0)
+fruit.shape("square")
+fruit.color("white")
+fruit.penup()
+fruit.goto(30, 30)
+
+old_fruit =[]
+
+# Scoring
+scoring = turtle.Turtle()
+scoring.speed(0)
+scoring.color("white")
+scoring.penup()
+scoring.hideturtle()
+scoring.goto(0, 300)
+scoring.write("Score:", align="center", font=("Courier", 24, "bold"))
+
+# Define how to move
+def snake_go_up():
+    if snake.direction !="down":
+        snake.direction ="up"
+
+def snake_go_down():
+    if snake.direction !="up":
+        snake.direction ="down"
+
+def snake_go_left():
+    if snake.direction !="right":
+        snake.direction ="left"
+
+def snake_go_right():
+    if snake.direction !="left":
+        snake.direction ="right"
